1935eb06c3b2cb2a10f60c951e79ca031e8b903a
curso <curso@curso.com>
2022-08-05 20:25:16 -0300

    implementado o fragmento de criaçao/edição das comidas

--- app/src/main/java/br/com/impacta/room/ui/fragments/CreateEditFragment.kt
+++ app/src/main/java/br/com/impacta/room/ui/fragments/CreateEditFragment.kt
@@ -5,14 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import br.com.impacta.room.R
+import br.com.impacta.room.data.models.Comida
 import br.com.impacta.room.databinding.FragmentCreateEditBinding
+import br.com.impacta.room.ui.activities.MainActivity
+import br.com.impacta.room.ui.viewModels.HomeViewModel
+import br.com.impacta.room.ui.viewModels.HomeViewModelFactory
 
 
 class CreateEditFragment : Fragment() {
 
     private var _binding: FragmentCreateEditBinding? = null
     private val binding get() = _binding!!
+    private val args: CreateEditFragmentArgs by navArgs()
+    private val viewModel: HomeViewModel by activityViewModels {
+        HomeViewModelFactory((activity as MainActivity).db)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +32,61 @@ class CreateEditFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentCreateEditBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (args.comidaId == -1) {
+            binding.button2.visibility = View.GONE
+
+            binding.button.setOnClickListener {
+                var comida = Comida(
+                    id = 2,
+                    nomeComida = binding.editTextTextPersonName.text.toString(),
+                    origem = binding.editTextTextPersonName2.text.toString(),
+                    calorias = binding.editTextTextPersonName3.text.toString().toInt(),
+                    tipo = binding.editTextTextPersonName4.text.toString(),
+                )
+
+                viewModel.criarComida(comida)
+
+                val action = CreateEditFragmentDirections.actionCreateEditFragmentToHomeFragment()
+                findNavController().navigate(action)
+            }
+        } else {
+            var comida: Comida? = null
+            viewModel.comida.observe(viewLifecycleOwner) { it ->
+                it?.let { comidaLiveData ->
+                    comida = comidaLiveData
+                    binding.comida = comidaLiveData
+                }
+            }
+            viewModel.recuperaComida(args.comidaId)
+
+            binding.button.setOnClickListener {
+                comida?.let {
+                    it.nomeComida = binding.editTextTextPersonName.text.toString()
+                    it.origem = binding.editTextTextPersonName2.text.toString()
+                    it.calorias = binding.editTextTextPersonName3.text.toString().toInt()
+                    it.tipo = binding.editTextTextPersonName4.text.toString()
+
+                    viewModel.atualizaComida(it)
+
+                    val action = CreateEditFragmentDirections.actionCreateEditFragmentToHomeFragment()
+                    findNavController().navigate(action)
+                }
+            }
+
+            binding.button2.setOnClickListener {
+                comida?.let{
+                    viewModel.deletaComida(it)
+
+                    val action = CreateEditFragmentDirections.actionCreateEditFragmentToHomeFragment()
+                    findNavController().navigate(action)
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/br/com/impacta/room/ui/viewModels/HomeViewModel.kt
+++ app/src/main/java/br/com/impacta/room/ui/viewModels/HomeViewModel.kt
@@ -11,12 +11,38 @@ class HomeViewModel(appDatabase: AppDatabase): ViewModel() {
 
     private val _listaComida: MutableLiveData<List<Comida>> = MutableLiveData(listOf())
     val listaComida: LiveData<List<Comida>> = _listaComida
+    private val _comida: MutableLiveData<Comida?> = MutableLiveData(null)
+    val comida: LiveData<Comida?> = _comida
 
     private val repository: ComidaRepository = ComidaRepository(appDatabase)
 
     fun getComidas() {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             _listaComida.postValue(repository.buscarTodas())
+        }
+    }
+
+    fun criarComida(comida: Comida) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.inserir(comida)
+        }
+    }
+
+    fun recuperaComida(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+           _comida.postValue(repository.buscarComidaPorId(id))
+        }
+    }
+
+    fun atualizaComida(comida: Comida) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.atualizar(comida)
+        }
+    }
+
+    fun deletaComida(comida: Comida) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deletar(comida)
         }
     }
 
